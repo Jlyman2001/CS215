8f06cb2113322b87b14437f4e5bac094c10d247e
Jlyman2001 <lymanj@sonoma.edu>
2024-09-16 14:36:24 -0700

    DNA project + file cleanup
    The testing code really didn't need to be on github and caused a bunch of needless branching. As such it is removed

--- Project 1 - DNA/Project 1 - DNA.cpp
+++ Project 1 - DNA/Project 1 - DNA.cpp
@@ -1,9 +1,22 @@
-// Project 1 - DNA.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* Project 1 - DNA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+* Version 1.1:
+*   Updated code after first working run to improve comments and code readability.
+* Version 1.0:
+*   Code fully functional, works when submitted on ZyBooks
+*   Completely reworked function to use erase() instead of weird indexing
+* Versions earlier than this did not get version numbers when worked on, more complete version history 
+* is available on github at https://github.com/Jlyman2001/CS215/tree/main
+* 
+* 
+* */
+
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
+
+
 /**
 * function to find the longest consecutive sequence of Short Tandem Repeats (STR)
 in a DNA sequence
@@ -13,7 +26,79 @@ in a DNA sequence
 * @returns the length of the longest consecutive sequence of STR in the DNA
 sequence
 **/
+int numOccurrences(string& STR, string sequence /* NOTE: Passed by copy!*/)
+{
+    //variable to count the longest repeated sequence of STRs
+    int longestSeq = 0;
 
+    //variable to count the length of current STR sequence in loop
+    int lengthOfCurrentSeq = 0;
+
+    //unused debug counting variable, used to trace loop execution
+    //int debugCount = 0;
+
+    //The loop iterates over the string, removing characters from the string until it is empty
+    while (sequence.length() > 0)
+    {
+        //debug tracing output code
+        //cout << "in while loop, step " << debugCount << endl;
+
+        //find the next hit for STR
+        int indexOfNextHit = sequence.find(STR,0);
+
+        //debug code for checking indexOfNextHit
+        //cout << indexOfNextHit << endl;
+        
+        //if there is no next hit for STR
+        if (indexOfNextHit == string::npos)
+        {
+            //The program would initially break here, this print was to detect when this branch executed
+            //cout << "NPOS" << endl;
+            
+            //if it doesn't repeat again, we can end execution here
+            return longestSeq;
+        }
+
+        //if the next STR is not at the start of the string
+        else if (indexOfNextHit != 0)
+        {
+            //Erase all characters up until the STR
+            sequence.erase(0, indexOfNextHit);
+            lengthOfCurrentSeq = 0;
+        }
+
+        //otherwise, the STR begins at the start of the string
+        else
+        {
+            //remove the first N characters of string, to remove next hit of STR
+            sequence.erase(0, STR.length());
+
+            //increment length of current sequence
+            lengthOfCurrentSeq = lengthOfCurrentSeq + 1;
+
+            //if this is now the longest sequence,
+            if (lengthOfCurrentSeq > longestSeq)
+            {
+                //update the longest sequence count to match
+                longestSeq = lengthOfCurrentSeq;
+            }
+        }
+        
+        //counter variable for debugging
+        //debugCount++;
+    }
+
+    //at this point, the string is empty, should only happen if string ends exactly on STR
+    //in case this happens, return the sequence number
+    return longestSeq;
+
+}
+
+
+//Old version
+//Remnant code preserved for archival purposes
+/*
+* 
 /*
 * Tasks:
 *   loop through string
@@ -25,10 +110,11 @@ sequence
 * 
 * Pseudocode:
 
-*/
 
+* 
 int numOccurrences(string& STR, string& sequence)
 {
+    
     //variable for final return value
     int longestChainOfOccurrences = 0;
 
@@ -88,11 +174,17 @@ int numOccurrences(string& STR, string& sequence)
     }
     return longestChainOfOccurrences;
 }
+*/
+
 
 int main(int argc, const char* argv[]) {
     // ****** STOP: DO NOT CHANGE THIS FUNCTION FOR THIS CHECKPOINT *******
     string sequence, STR; //strings storing the DNA sequence and the Short Tandem Repeats(STR)
         //Read sequence and STR from standard input
+    
+    //hard code for testing
+    //sequence = "AGACGGGTTACCATGACTATCTATCTATCTATCTATCTATCTATCTATCACGTACGTACGTATCGAGATAGATAGATAGATAGATCCTCGACTTCGATCGCAATGAATGCCAATAGACAAAA";
+    //STR = "AGAT";
     cin >> sequence;
     cin >> STR;
 
@@ -101,6 +193,8 @@ int main(int argc, const char* argv[]) {
     
     //Print out the length
     cout << num << endl;
+    //cout << sequence << endl;
+
 }
 
 
